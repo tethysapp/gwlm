7da4ad19048671c2a124599f9d22fc5e520fcbd5
SarvaPulla <sarvapulla@gmail.com>
2020-06-23 21:26:10 -0500

    Interpolation WIP

--- tethysapp/gwlm/public/js/region_map.js
+++ tethysapp/gwlm/public/js/region_map.js
@@ -155,6 +155,7 @@ var LIBRARY_OBJECT = (function() {
             cache:48
         });
         tdWmsLayer.addTo(map);
+        map.removeLayer(tdWmsLayer);
 
         aquiferGroup = L.featureGroup().addTo(map);
         markers = L.markerClusterGroup(    {iconCreateFunction: function (cluster) {
@@ -197,7 +198,7 @@ var LIBRARY_OBJECT = (function() {
         var min_input = L.control({position: 'topright'});
         min_input.onAdd = function(map){
             var div = L.DomUtil.create('div', 'min_input');
-            div.innerHTML = '<b>Min:</b><input type="number" class="form-control input-sm" name="leg_min" id="leg_min" min="-5000" max="5000" step="10" value="-500">';
+            div.innerHTML = '<b>Min:</b><input type="number" class="form-control input-sm" name="leg_min" id="leg_min" min="-5000" max="5000" step="10" value="-500" disabled>';
             return div;
         };
         min_input.addTo(map);
@@ -206,7 +207,7 @@ var LIBRARY_OBJECT = (function() {
         max_input.onAdd = function(map){
             var div = L.DomUtil.create('div', 'max_input');
             div.innerHTML = '<b>Max:</b><input type="number" class="form-control input-sm" name="leg_max" id="leg_max" ' +
-                'min="-5000" max="5000" step="10" value="0">';
+                'min="-5000" max="5000" step="10" value="0" disabled>';
             return div;
         };
         max_input.addTo(map);
@@ -231,7 +232,7 @@ var LIBRARY_OBJECT = (function() {
         opacity_input.onAdd = function(map){
             var div = L.DomUtil.create('div', 'opacity_input');
             div.innerHTML = '<b>Opacity:</b><input type="number" class="form-control input-sm" name="opacity" id="opacity_val" ' +
-                'min="0" max="1" step="0.1" value="0.7">';
+                'min="0" max="1" step="0.1" value="1.0">';
             return div;
         };
         opacity_input.addTo(map);
@@ -263,7 +264,7 @@ var LIBRARY_OBJECT = (function() {
                 popupString += k + ': ' + v + '<br />';
             }
             // console.log(well_obs[well_id]);
-            popupString += '<a class="btn btn-default set-outlier" id="set-outlier" name="set-outlier">Set/Unset as Outlier</a></div>';
+            popupString += '<a class="btn btn-default set-outlier" id="set-outlier" name="set-outlier">Outlier</a></div>';
             layer.bindPopup(popupString);
             layer.on('click', get_ts);
         }
@@ -440,10 +441,11 @@ var LIBRARY_OBJECT = (function() {
         var xhr = ajax_update_database("get-wms-datasets", data);
         xhr.done(function(return_data) {
             if ("success" in return_data) {
-                console.log(return_data);
                 $("#select-interpolation").html('');
+                // $("#select-interpolation").prop("selected", false);
                 var empty_opt = '<option value="" selected disabled>Select item...</option>';
                 $("#select-interpolation").append(empty_opt);
+                $("#select-interpolation").val('').trigger('change');
                 var wms_options = return_data['wms_files'];
                 wms_options.forEach(function(attr,i){
                     var wms_option = new Option(attr[1], attr[0]);
@@ -460,7 +462,6 @@ var LIBRARY_OBJECT = (function() {
             if("success" in return_data){
                 well_obs = return_data['obs_dict'];
 
-                // console.log(Object.keys(well_obs).length);
                 if('min_obs' in return_data){
                     min_obs = return_data['min_obs'];
                     max_obs = return_data['max_obs'];
@@ -492,7 +493,6 @@ var LIBRARY_OBJECT = (function() {
         var xhr = ajax_update_database("get-wms-metadata", data);
         xhr.done(function(return_data){
             if("success" in return_data) {
-                console.log(return_data);
                 var range_min = return_data['range_min'];
                 var range_max = return_data['range_max'];
                 $("#leg_min").val(range_min);
@@ -510,6 +510,7 @@ var LIBRARY_OBJECT = (function() {
             format: 'image/png',
             transparent: true,
             styles: 'boxfill/'+style,
+            opacity: '1.0',
             colorscalerange: [range_min, range_max],
             version:'1.3.0'
         });
@@ -579,27 +580,49 @@ var LIBRARY_OBJECT = (function() {
             get_well_obs(aquifer_id, variable_id);
             original_map_chart();
             get_wms_datasets(aquifer_name, variable_id, region);
+            $("#legend-image").attr("src", '');
+            map.removeLayer(tdWmsLayer);
         }).change();
 
         $("#variable-select").change(function(){
             var aquifer_id = $("#aquifer-select option:selected").val();
             var variable_id = $("#variable-select option:selected").val();
+            var region = $("#region-text-input").val();
+
             get_well_obs(aquifer_id, variable_id);
             original_map_chart();
+            get_wms_datasets(aquifer_name, variable_id, region);
+            $("#legend-image").attr("src", '');
+            map.removeLayer(tdWmsLayer);
         });
 
         $("#select-interpolation").change(function(){
             var wms_endpoint = $("#select-interpolation option:selected").val();
             var file_name = $("#select-interpolation option:selected").text();
-            var aquifer_name = $("#aquifer-select option:selected").text();
-            var region = $("#region-text-input").val();
-            get_wms_metadata(region, aquifer_name, file_name, wms_endpoint);
+            if(file_name!=='Select item...'){
+                var aquifer_name = $("#aquifer-select option:selected").text();
+                var region = $("#region-text-input").val();
+                get_wms_metadata(region, aquifer_name, file_name, wms_endpoint);
+            }
+
         });
 
         $("#select_symbology").change(function(){
             var symbology = $("#select_symbology option:selected").val();
             wmsLayer.setParams({styles: 'boxfill/'+symbology});
+            var wmsUrl = wmsLayer._url;
+            var range_min = wmsLayer.wmsParams.colorscalerange[0];
+            var range_max = wmsLayer.wmsParams.colorscalerange[1];
+            var src = wmsUrl + "?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetLegendGraphic&LAYER=tsvalue"+
+                "&colorscalerange="+range_min+","+range_max+"&PALETTE="+symbology+"&transparent=TRUE";
+            $("#legend-image").attr("src", src);
         });
+
+        $("#opacity_val").change(function(){
+           var opacity = $("#opacity_val").val();
+           tdWmsLayer.setOpacity(opacity);
+        });
+
 
         slidervar.noUiSlider.on('update', function( values, handle ) {
 
